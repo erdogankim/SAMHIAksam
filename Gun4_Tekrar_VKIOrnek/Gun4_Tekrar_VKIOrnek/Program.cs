bc73fe1984aa8bffc2547f695b711e8432cae4d5
Erdğan ÖZTÜRK <ezterk@gmail.com>
2024-04-22 21:01:15 +0300

    VKI Algoritma yazıldı

--- Gun4_Tekrar_VKIOrnek/Gun4_Tekrar_VKIOrnek/Program.cs
+++ Gun4_Tekrar_VKIOrnek/Gun4_Tekrar_VKIOrnek/Program.cs
@@ -9,4 +9,27 @@
  */
 
 
+Console.Write("Kilo bilgisini giriniz (Kg) ..:");
+string strKilo = Console.ReadLine();
+double kilo = Convert.ToDouble(strKilo);
+
+Console.Write("Boy bilgisini giriniz (Boy) ..:");
+string  strBoy = Console.ReadLine();
+double boy = Convert.ToDouble(strBoy);
+
+Console.WriteLine("Cinsiyet bilgini giriniz [K/E] ..:");
+string cinsiyet = Console.ReadLine();
+
+double katSayi = 1.2;
+if (cinsiyet == "K")
+{
+    katSayi = 0.8;
+}
+
+//double VKI = (kilo / (boy*boy)) * katSayi;
+double VKI = (kilo / Math.Pow(boy,2)) * katSayi;
+
+Console.WriteLine(VKI);
+
+
 
