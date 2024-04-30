0afbaa6dede44fbc837ee2500ad83dc281fe91c5
Erdğan ÖZTÜRK <ezterk@gmail.com>
2024-04-30 21:20:38 +0300

    String ifadeler

--- Gun7_Tekrar_String/Gun7_Tekrar_String/Program.cs
+++ Gun7_Tekrar_String/Gun7_Tekrar_String/Program.cs
@@ -62,6 +62,18 @@
 //    Console.WriteLine();
 //}
 
+
+// Kelime dışarıdan girilmek üzere (Örn: KONYA)
+// K
+// KO
+// KON
+// KONY
+// KONYA
+
+
+
+
+
 // Piglatin bir kelime oyunudur. Belirlenmiş bir kelimenin son harfi 
 // alınıp en başa konulup yeni bir kelime üreterek oynanır. 
 // Harfleri başa ala ala ilk yazılan kelimeye gelindiğinde oyun biter 
@@ -80,14 +92,23 @@
 // Soru: Kelime dışarıdan girildiğinde bu şekilde piglatin döngüsünü 
 // gösteren programı yazınız. 
 
-string kelime = "ANTALYA";
-Console.WriteLine(kelime);
+//string kelime = "ANTALYA";
+//Console.WriteLine(kelime);
 
-string subKelime = kelime.Substring(0, 4);
-Console.WriteLine(subKelime);
+//string subKelime = kelime.Substring(0, 4);
+//Console.WriteLine(subKelime);
 
-string subKelime2 = kelime.Substring(3);
-Console.WriteLine(subKelime2);
+//string subKelime2 = kelime.Substring(3);
+//Console.WriteLine(subKelime2);
 
-int karakterSayisi = kelime.Length;
-Console.WriteLine(karakterSayisi);
+//int karakterSayisi = kelime.Length;
+//Console.WriteLine(karakterSayisi);
+
+Console.Write("Kelimeyi giriniz ...:");
+string kelime = Console.ReadLine();
+
+for (int i = 0; i < kelime.Length; i++)
+{
+    string strSubKelime = kelime.Substring(0, i + 1);
+    Console.WriteLine(strSubKelime);
+}
