ce7a89edd0a51091bcddb345e4ee8af25fa7c864
erdogankim <167590307+erdogankim@users.noreply.github.com>
2024-05-14 20:25:00 +0300

    örnek

--- Gun12_CokBoyutlu_Dagitik_DiziDegiskenler/Gun12_CokBoyutlu_Dagitik_DiziDegiskenler/Program.cs
+++ Gun12_CokBoyutlu_Dagitik_DiziDegiskenler/Gun12_CokBoyutlu_Dagitik_DiziDegiskenler/Program.cs
@@ -3,7 +3,7 @@
 int[] myArr = new int[5];
 for (int i = 0; i < myArr.Length; i++)
 {
-    myArr[i] = rnd.Next(1,100);
+    myArr[i] = rnd.Next(1, 100);
 }
 
 
@@ -37,3 +37,46 @@ for (int i = 0; i < arr.Length; i++)
         arr[i][j] = rnd.Next(1, 100);
     }
 }
+
+
+// Okulun, beden eğitimi dersleri için sınıfa verilecek olan 
+// basketbol, futbol ve voleybol top adetleri ve öğrenci isimlerinin 
+// listesi şeklinde bir kayıt bütünü oluşturan programı geliştiriyoruz
+
+
+// Basketbol top adedi
+// voleybol top adedi
+// futbol top adedi 
+// öğrencilerin listesi
+
+// Öğrenci sayısını giriniz ...: 5
+// (5 öğreci için isim ve soyisimleri dışarıdan tekrar girilecek) 
+
+// Basketbol topu sayısınız giriniz ...: 1
+// Voleybol topu sayısını giriniz ...: 1
+// Futbol topu sayısını giriniz ...: 2
+
+Console.Write("Öğrenci sayısını giriniz ...:");
+int ogrSayisi = Convert.ToInt32(Console.ReadLine());
+
+string[][] data = new string[4][];
+data[0] = new string[ogrSayisi];
+data[1] = new string[1];
+data[2] = new string[1];
+data[3] = new string[1];
+
+for (int i = 0;i < data[0].Length; i++)
+{
+    Console.Write("Ad Soyad bilgisini giriniz ....:");
+    data[0][i] = Console.ReadLine();
+}
+
+Console.Write("Basket topu sayısını giriniz ...:");
+data[1][0] = Console.ReadLine();
+
+Console.Write("Volleybol topu sayısını giriniz ...:");
+data[2][0] = Console.ReadLine();
+
+Console.Write("Futbol topu sayısını giriniz ...:");
+data[3][0] = Console.ReadLine();
+
