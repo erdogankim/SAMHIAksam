eba79a16e1e3d32858cde02eed7d190e04a15409
erdogankim <167590307+erdogankim@users.noreply.github.com>
2024-05-14 20:02:54 +0300

    Ornek

---
+++ Gun12_CokBoyutlu_Dagitik_DiziDegiskenler/Gun12_CokBoyutlu_Dagitik_DiziDegiskenler/Program.cs
@@ -0,0 +1,39 @@
+﻿Random rnd = new Random();
+// Düzenli dizi değişkenler
+int[] myArr = new int[5];
+for (int i = 0; i < myArr.Length; i++)
+{
+    myArr[i] = rnd.Next(1,100);
+}
+
+
+int[,] myMultiDimensionalArray = new int[2, 2];
+for (int i = 0; i < myMultiDimensionalArray.Length; i++)
+{
+    for (int j = 0; j < myMultiDimensionalArray.GetLongLength(1); j++)
+    {
+
+    }
+}
+
+
+int[,,] my3DimensionalArray = new int[2, 2, 2];
+
+int[,,,] my4DimensionalArray = new int[2, 2, 2, 2];
+
+int[,,,,] my5DimensionalArray = new int[2, 2, 2, 2, 2];
+
+// Düzensiz dizi değişken
+
+int[][] arr = new int[3][];
+arr[0] = new int[3];
+arr[1] = new int[1];
+arr[2] = new int[2];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < arr[i].Length; j++)
+    {
+        arr[i][j] = rnd.Next(1, 100);
+    }
+}
