f05049b5db586cc6a78e1c335abf8dbaa3113480
erdogankim <167590307+erdogankim@users.noreply.github.com>
2024-05-27 21:34:08 +0300

    OOP Ozellikler

---
+++ Gun16_OOP_OzellikOrnek/Gun16_OOP_OzellikOrnek/Program.cs
@@ -0,0 +1,58 @@
+﻿
+// Abone adında bir class oluşturun. 
+// Bu class ın içerisinde, sonindex, ilkindex ve kullanım yeri özellikleri olsun.
+
+// AboneIslem adında bir class oluşturun. 
+// içinde FaturaHesapla adında bir method olsun. 
+// Bu method geriye double olarak hesaplanmış faturayı döndürsün. 
+// Parametre olarak da Abone alsın. 
+// Dışarıdan girilen değerlerle
+// KullanımYeri => Mesken = 1.13, Umumi = 0.7
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Abone abone = new Abone();
+        Console.Write("Son index değerini giriniz ...:");
+        abone.SonIndex = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("İlk index değerini giriniz ...:");
+        abone.IlkIndex = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Kullanım yerini giriniz ...:");
+        abone.KullanimYeri= Console.ReadLine();
+
+        AboneIslem aboneIslem = new AboneIslem();
+        double faturaTutari = aboneIslem.FaturaHesapla(abone);
+        Console.WriteLine(faturaTutari);
+
+    }
+}
+
+public class Abone
+{
+    public int SonIndex { get; set; }
+    public int IlkIndex { get; set; }
+    public string KullanimYeri { get; set; }
+}
+
+public class AboneIslem
+{
+    public double FaturaHesapla(Abone a)
+    {
+        double birimFiyat = 0;
+        if (a.KullanimYeri == "Mesken")
+        {
+            birimFiyat = 1.13;
+        }
+        else
+        {
+            birimFiyat = 0.7;
+        }
+
+        double kullanilanSuMiktari = a.SonIndex - a.IlkIndex;
+        double toplam = birimFiyat * kullanilanSuMiktari;
+        return toplam;
+    }
+}
---
+++ Gun16_OOP_Properties/Gun16_OOP_Properties/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Telephone samsung = new Telephone();
+        samsung.ScreenSize = 5.3;
+
+        Personel isci = new Personel();
+        isci.Ad = "Ahmet";
+        isci.Soyad = "Demir";
+        isci.TCKN = "1111111111";
+
+        Personel mudur = new Personel();
+        mudur.Ad = "Mehmet";
+        mudur.Soyad = "Demir";
+        mudur.TCKN = "2222222222";
+    }
+}
+
+// Abone adında bir class oluşturun. 
+// Bu class ın içerisinde, sonindex, ilkindex ve kullanım yeri özellikleri olsun. 
+
+public class Telephone
+{
+    public double ScreenSize { get; set; }
+
+    public Telephone()
+    {
+
+    }
+
+    ~Telephone()
+    {
+
+    }
+
+}
+
+public class Personel
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public string TCKN { get; set; }
+}
