df8fbd1a182d737d3c2f79aa36a2753a256a940a
erdogankim <167590307+erdogankim@users.noreply.github.com>
2024-06-06 19:49:16 +0300

    Dosya Ornek

---
+++ Gun18_OOP_Ornek/Gun18_OOP_Ornek/DosyaOperasyon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Gun18_OOP_Ornek
+{
+    public class DosyaOperasyon
+    {
+        public void YeniOlustur(string dosyaYolu)
+        {
+            bool durum = File.Exists(dosyaYolu);
+            if (!durum)
+            {
+                File.Create(dosyaYolu);
+            }
+        }
+    }
+}
---
+++ Gun18_OOP_Ornek/Gun18_OOP_Ornek/Program.cs
@@ -0,0 +1,26 @@
+﻿// Bir adet dosyaoperasyon class ı oluşturun. 
+// Bu class YeniOlustur adinda bir method barındırsın. 
+// bu method parametre olarak "DosyaYolu" adında string değer alsın.
+// Geriye herhangi bir değer döndürmesin. 
+
+// Method çağırıldığında, string parametre olarak verilen DosyaYolu değerindeki
+// Fiziksel dosya yoluna yeni bir dosya oluştursun. 
+
+// YeniOlustur("c:\Projects\benimyenidosya.txt");
+
+
+using Gun18_OOP_Ornek;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Lütfen oluşturmak istediğiniz dosya yolu ve adını giriniz ...:");
+        string yol = Console.ReadLine();
+
+        DosyaOperasyon operasyon = new DosyaOperasyon();
+        operasyon.YeniOlustur(yol);
+
+        Console.WriteLine("Dosya oluşturuldu.");
+    }
+}
