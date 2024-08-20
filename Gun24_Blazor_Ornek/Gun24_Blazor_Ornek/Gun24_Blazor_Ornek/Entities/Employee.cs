fe7031279f1d6b7e0d37476377fc18ae676d5d8c
erdogankim <167590307+erdogankim@users.noreply.github.com>
2024-08-20 19:37:22 +0300

    Gun24-25

---
+++ Gun24_Blazor_Ornek/Gun24_Blazor_Ornek/Gun24_Blazor_Ornek/Entities/Employee.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gun24_Blazor_Ornek.Entities
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string FirstName { get; set; }
+
+        [StringLength(30)]
+        public string Title { get; set; }
+
+        [StringLength(25)]
+        public string TitleOfCourtesy { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public DateTime? HireDate { get; set; }
+
+        [StringLength(60)]
+        public string Address { get; set; }
+
+        [StringLength(15)]
+        public string City { get; set; }
+
+        [StringLength(15)]
+        public string Region { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(15)]
+        public string Country { get; set; }
+
+        [StringLength(24)]
+        public string HomePhone { get; set; }
+
+        [StringLength(4)]
+        public string Extension { get; set; }
+
+        public byte[] Photo { get; set; }  // For [image] data type
+
+        public string Notes { get; set; }  // For [ntext] data type
+
+        public int? ReportsTo { get; set; }
+
+        [StringLength(255)]
+        public string PhotoPath { get; set; }
+    }
+}
---
+++ Gun24_Blazor_Ornek/Gun24_Blazor_Ornek/Gun24_Blazor_Ornek/Entities/NorthwindDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gun24_Blazor_Ornek.Entities
+{
+    public class NorthwindDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog=Northwind; User Id=sa; Password=1; TrustServerCertificate=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
