5d1037610eb4028dd8786811c63590bfd52739f0
erdogankim <167590307+erdogankim@users.noreply.github.com>
2024-08-26 20:43:08 +0300

    Gun 28

---
+++ Gun28_Web_MVC/Gun28_Web_MVC/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gun28_Web_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public string Add()
+        {
+            return "Ekle methodu çalıştı";
+        }
+
+        public string Update(int id)
+        {
+            int carpim = id * id;
+            return "Çarpım değeri = " + carpim;
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gun28_Web_MVC/Gun28_Web_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gun28_Web_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
