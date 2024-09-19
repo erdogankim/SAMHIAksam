20024da3c7629f68800203196486794a767035e8
erdogankim <167590307+erdogankim@users.noreply.github.com>
2024-09-20 01:25:57 +0300

    güncelleme

---
+++ Gun30_MVCFeaturesAndPartials/Gun30_MVCFeaturesAndPartials/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Gun30_MVCFeaturesAndPartials.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gun30_MVCFeaturesAndPartials.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindDbContext _context;
+
+        public CategoryController(NorthwindDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
